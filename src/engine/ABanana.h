98159da34ab085675302083fd2cbaf11084e3a9c
MegaMech <MegaMech@users.noreply.github.com>
2024-09-09 01:41:20 -0600

    Update ABanana.h

--- src/engine/ABanana.h
+++ src/engine/ABanana.h
@@ -8,17 +8,7 @@
 class ABanana : public GameActor {
 public:
     uint8_t uuid[16];
-    int16_t type = 6; // ACTOR_BANANA
-    int16_t flags;
-    int16_t unk_04;
-    int16_t state;
-    float unk_08;
-    float boundingBoxSize;
-    s16 rot[3];
-    int16_t unk_16;
-    float pos[3];
-    float velocity[3];
-   // Collision unk30;
+    struct BananaActor a;
 
    // virtual ~GameActor() = default;  // Virtual destructor for proper cleanup in derived classes
 
