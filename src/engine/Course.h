d786cb632acf43b0332b9ce4c6bc869bb7dc8203
MegaMech <7255464+MegaMech@users.noreply.github.com>
2024-09-12 20:11:35 -0600

    properties

--- src/engine/Course.h
+++ src/engine/Course.h
@@ -12,6 +12,9 @@ extern "C" {
     #include "camera.h"
     #include "course_offsets.h"
     #include "data/some_data.h"
+    #include "defines.h"
+    #include "bomb_kart.h"
+    #include "path_spawn_metadata.h"
 void CourseManager_LoadCourse(const char* courseVtx, 
                       course_texture* textures, const char* displaylists, size_t dlSize);
 
@@ -53,6 +56,36 @@ public:
     virtual void Collision();
     virtual void GenerateCollision();
     virtual void Destroy();
+
+    const char* name = "Course Name";
+    const char* debugName = "CName";
+    const char* courseLength = "567m";
+    uint32_t cup = FLOWER_CUP;
+    uint32_t cupIndex = 3;
+    const char* KartAIBehaviour = D_0D008F28;
+    float KartAIMaximumseparation = 50.0f;
+    float MinimumAISeparation = 0.3f;
+    int16_t *somePtr = D_800DCB34;
+    uint32_t AISteeringSensitivity = 48;
+    BombKartSpawn gBombKartSpawns[7] = {
+        { 40, 3, 0.8333333, 0, 0, 0, 0 },
+        { 100, 3, 0.8333333, 0, 0, 0, 0 },
+        { 265, 3, 0.8333333, 0, 0, 0, 0 },
+        { 285, 1, 0.8333333, 0, 0, 0, 0 },
+        { 420, 1, 0.8333333, 0, 0, 0, 0 },
+        { 0, 0, 0.8333333, 0, 0, 0, 0 },
+        { 0, 0, 0.8333333, 0, 0, 0, 0 },
+    };
+    struct _struct_gCoursePathSizes_0x10 PathSizes = {600, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0};
+
+    Vec4f D_0D009418 = {4.1666665f, 5.5833334f, 6.1666665f, 6.75f};
+    Vec4f D_0D009568 = {3.75f, 5.1666665f, 5.75f, 6.3333334f};
+    Vec4f D_0D0096B8 = {3.3333332f, 3.9166667f, 4.5f, 5.0833334f};
+    Vec4f D_0D009808 = {3.75f, 5.1666665f, 5.75f, 6.3333334f};
+
+    const char *PathTable[4] = {
+        nullptr, nullptr, nullptr, nullptr
+    };
 
     CloudData *clouds = gKalimariDesertClouds;
     CloudData *cloudList = gLuigiRacewayClouds;
--- src/engine/MarioRaceway.h
+++ src/engine/MarioRaceway.h
@@ -10,6 +10,8 @@ extern "C" {
     #include "camera.h"
     #include "data/some_data.h"
     #include "objects.h"
+    #include "path_spawn_metadata.h"
+    #include "mario_raceway_data.h"
     extern const course_texture mario_raceway_textures[];
 }
 
@@ -40,6 +42,37 @@ public:
     virtual void Collision() override;
     virtual void GenerateCollision() override;
     virtual void Destroy() override;
+
+    const char* name = "Mario Raceway";
+    const char* debugName = "m circuit";
+    const char* courseLength = "567m";
+    uint32_t cup = FLOWER_CUP;
+    uint32_t cupIndex = 3;
+    const char* KartAIBehaviour = D_0D008F28;
+    float KartAIMaximumseparation = 50.0f;
+    float MinimumAISeparation = 0.3f;
+    int16_t *somePtr = D_800DCB34;
+    uint32_t AISteeringSensitivity = 48;
+    BombKartSpawn BombKartSpawns[7] = {
+        { 40, 3, 0.8333333, 0, 0, 0, 0 },
+        { 100, 3, 0.8333333, 0, 0, 0, 0 },
+        { 265, 3, 0.8333333, 0, 0, 0, 0 },
+        { 285, 1, 0.8333333, 0, 0, 0, 0 },
+        { 420, 1, 0.8333333, 0, 0, 0, 0 },
+        { 0, 0, 0.8333333, 0, 0, 0, 0 },
+        { 0, 0, 0.8333333, 0, 0, 0, 0 },
+    };
+    struct _struct_gCoursePathSizes_0x10 PathSizes = {600, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0};
+
+    Vec4f D_0D009418 = {4.1666665f, 5.5833334f, 6.1666665f, 6.75f};
+    Vec4f D_0D009568 = {3.75f, 5.1666665f, 5.75f, 6.3333334f};
+    Vec4f D_0D0096B8 = {3.3333332f, 3.9166667f, 4.5f, 5.0833334f};
+    Vec4f D_0D009808 = {3.75f, 5.1666665f, 5.75f, 6.3333334f};
+
+    const char* PathTable[4] = {
+        d_course_mario_raceway_unknown_waypoints,
+        nullptr, nullptr, nullptr
+    };
 
     CloudData *clouds = gKalimariDesertClouds;
     CloudData *cloudList = gLuigiRacewayClouds;
