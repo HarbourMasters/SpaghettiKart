86d56698b24e4398e113194a265dbfb03791487e
MegaMech <MegaMech@users.noreply.github.com>
2025-12-19 07:36:35 -0700

    Update code_8006E9C0.c

--- src/code_8006E9C0.c
+++ src/code_8006E9C0.c
@@ -653,8 +653,8 @@ void init_hud_one_player(void) {
     D_8018D150 = 0;
     D_8018CFCC = 1.0f;
     find_unused_obj_index(&D_80183DA0);
-    find_unused_obj_index(&gIndexLakituList[0]);
-    find_unused_obj_index(&gIndexLakituList[1]);
+    //find_unused_obj_index(&gIndexLakituList[0]);
+    //find_unused_obj_index(&gIndexLakituList[1]);
     find_unused_obj_index(&gItemWindowObjectByPlayerId[0]);
     find_unused_obj_index(&gItemWindowObjectByPlayerId[1]);
     init_object_list_index();
@@ -732,8 +732,8 @@ void init_hud_one_player(void) {
 void init_hud_two_player_vertical(void) {
     find_unused_obj_index(&D_80183DA0);
 
-    find_unused_obj_index(&gIndexLakituList[0]);
-    find_unused_obj_index(&gIndexLakituList[1]);
+    //find_unused_obj_index(&gIndexLakituList[0]);
+    //find_unused_obj_index(&gIndexLakituList[1]);
 
     find_unused_obj_index(&gItemWindowObjectByPlayerId[0]);
     find_unused_obj_index(&gItemWindowObjectByPlayerId[1]);
@@ -800,8 +800,8 @@ void init_hud_two_player_vertical(void) {
 void init_hud_two_player_horizontal() {
     find_unused_obj_index(&D_80183DA0);
 
-    find_unused_obj_index(&gIndexLakituList[0]);
-    find_unused_obj_index(&gIndexLakituList[1]);
+    //find_unused_obj_index(&gIndexLakituList[0]);
+    //find_unused_obj_index(&gIndexLakituList[1]);
 
     find_unused_obj_index(&gItemWindowObjectByPlayerId[0]);
     find_unused_obj_index(&gItemWindowObjectByPlayerId[1]);
@@ -874,10 +874,10 @@ void init_hud_two_player_horizontal() {
 void init_hud_three_four_player(void) {
     find_unused_obj_index(&D_80183DA0);
 
-    find_unused_obj_index(&gIndexLakituList[0]);
-    find_unused_obj_index(&gIndexLakituList[1]);
-    find_unused_obj_index(&gIndexLakituList[2]);
-    find_unused_obj_index(&gIndexLakituList[3]);
+    //find_unused_obj_index(&gIndexLakituList[0]);
+    //find_unused_obj_index(&gIndexLakituList[1]);
+    //find_unused_obj_index(&gIndexLakituList[2]);
+    //find_unused_obj_index(&gIndexLakituList[3]);
 
     find_unused_obj_index(&gItemWindowObjectByPlayerId[0]);
     find_unused_obj_index(&gItemWindowObjectByPlayerId[1]);
